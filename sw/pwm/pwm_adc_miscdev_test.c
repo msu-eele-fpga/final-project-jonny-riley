fa80b87ffa1013d7fcca168dbec8b7f241ab8209
Riley Holmes <RileyHolmes1010@gmail.com>
2024-12-10 21:34:47 -0700

    pwm adc miscdev code revision for code quality

--- sw/pwm/pwm_adc_miscdev_test.c
+++ sw/pwm/pwm_adc_miscdev_test.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <unistd.h>
 
-// TODO: update these offsets if your address are different
+
 #define CH0 0x00
 #define CH1 0x04
 #define CH2 0x08
@@ -15,12 +15,14 @@
 #define RED_DUTY_CYCLE_OFFSET 0x08
 #define GREEN_DUTY_CYCLE_OFFSET 0x0C
 
-uint32_t convert (uint32_t adc_value){
+
+
+uint32_t convert (uint32_t adc_value){				//to convert adc_value to pwm duty cycle
 	uint32_t max_duty = 2 << 17;
 	uint32_t max_adc = 2 << 12;
+	
 	uint32_t duty_adc_ratio = max_duty/max_adc;
 	uint32_t result = adc_value * duty_adc_ratio;
-	//result = result << 5;
 
 	return(result);
 }
@@ -46,40 +48,12 @@ int main () {
 		exit(1);
 	}
 
-	// Test reading the registers sequentially
-	printf("\n************************************\n*");
-	printf("* read initial register values\n");
-	printf("************************************\n\n");
 
-	pwm_ret = fread(&val, 4, 1, pwm_file);
-	printf("Period  = 0x%x\n", val);
-
-	pwm_ret = fread(&val, 4, 1, pwm_file);
-	printf("Blue Duty Cycle = 0x%x\n", val);
-
-	pwm_ret = fread(&val, 4, 1, pwm_file);
-	printf("Red Duty Cycle = 0x%x\n", val);
-
-	pwm_ret = fread(&val, 4, 1, pwm_file);
-	printf("Green Duty Cycle = 0x%x\n", val);
-
-	// Reset file position to 0
-	pwm_ret = fseek(pwm_file, 0, SEEK_SET);
-	printf("fseek ret = %d\n", pwm_ret);
-	printf("errno =%s\n", strerror(errno));
-
-	val = 0x00;
-    //pwm_ret = fseek(pwm_file, HPS_LED_CONTROL_OFFSET, SEEK_SET);
-	//ret = fwrite(&val, 4, 1, pwm_file);
-	// We need to "flush" so the OS finishes writing to the file before our code continues.
-	fflush(pwm_file);
-
-	
 	printf("\nControlling the led with the adc\n\n");
 	
 
 	while(1){
-		//Blue adc write
+		//Blue LED Control
 		adc_ret = fseek(adc_file, CH0, SEEK_SET);
 		adc_ret = fread(&val, 4, 1, adc_file);
 		scaled_val = convert(val);
@@ -87,7 +61,7 @@ int main () {
 		pwm_ret = fwrite(&scaled_val, 4, 1, pwm_file);
 		fflush(pwm_file);
 
-		//Red adc write
+		//Red LED Control
 		adc_ret = fseek(adc_file, CH1, SEEK_SET);
 		adc_ret = fread(&val, 4, 1, adc_file);
 		scaled_val = convert(val);
@@ -95,7 +69,7 @@ int main () {
 		pwm_ret = fwrite(&scaled_val, 4, 1, pwm_file);
 		fflush(pwm_file);
 
-		//Green adc write
+		//Green LED Control
 		adc_ret = fseek(adc_file, CH2, SEEK_SET);
 		adc_ret = fread(&val, 4, 1, adc_file);
 		scaled_val = convert(val);
@@ -105,93 +79,6 @@ int main () {
 
 
 	}
-
-	// Turn on hardware-control mode
-	//printf("back to hardware-control mode....\n");
-	//val = 0x01;
-    //ret = fseek(file, HPS_LED_CONTROL_OFFSET, SEEK_SET);
-	//ret = fwrite(&val, 4, 1, file);
-	//fflush(file);
-
-	/*
-	printf("\n************************************\n*");
-	printf("* write values\n");
-	printf("************************************\n\n");
-	// Turn on software-control mode
-	val = 0x00;
-    ret = fseek(file, HPS_LED_CONTROL_OFFSET, SEEK_SET);
-	ret = fwrite(&val, 4, 1, file);
-	// We need to "flush" so the OS finishes writing to the file before our code continues.
-	fflush(file);
-
-	// Write some values to the LEDs
-	printf("writing patterns to LEDs....\n");
-	val = 0x55;
-    ret = fseek(file, LED_REG_OFFSET, SEEK_SET);
-	ret = fwrite(&val, 4, 1, file);
-	fflush(file);
-
-	sleep(1);
-
-	val = 0xaa;
-    ret = fseek(file, LED_REG_OFFSET, SEEK_SET);
-	ret = fwrite(&val, 4, 1, file);
-	fflush(file);
-
-	sleep(1);
-
-	val = 0xff;
-    ret = fseek(file, LED_REG_OFFSET, SEEK_SET);
-	ret = fwrite(&val, 4, 1, file);
-	fflush(file);
-
-	usleep(0.5e6);
-
-	val = 0x00;
-    ret = fseek(file, LED_REG_OFFSET, SEEK_SET);
-	ret = fwrite(&val, 4, 1, file);
-	fflush(file);
-
-	sleep(1);
-
-	// Turn on hardware-control mode
-	printf("back to hardware-control mode....\n");
-	val = 0x01;
-    ret = fseek(file, HPS_LED_CONTROL_OFFSET, SEEK_SET);
-	ret = fwrite(&val, 4, 1, file);
-	fflush(file);
-
-	val = 0x12;
-    ret = fseek(file, BASE_PERIOD_OFFSET, SEEK_SET);
-	ret = fwrite(&val, 4, 1, file);
-	fflush(file);
-
-	sleep(5);
-
-	// Speed up the base period!
-	val = 0x02;
-    ret = fseek(file, BASE_PERIOD_OFFSET, SEEK_SET);
-	ret = fwrite(&val, 4, 1, file);
-	fflush(file);
-
-
-	printf("\n************************************\n*");
-	printf("* read new register values\n");
-	printf("************************************\n\n");
-	
-	// Reset file position to 0
-	ret = fseek(file, 0, SEEK_SET);
-
-	ret = fread(&val, 4, 1, file);
-	printf("HPS_LED_control = 0x%x\n", val);
-
-	ret = fread(&val, 4, 1, file);
-	printf("base period = 0x%x\n", val);
-
-	ret = fread(&val, 4, 1, file);
-	printf("LED_reg = 0x%x\n", val);
-
-	*/
 
 	fclose(pwm_file);
 	fclose(adc_file);
